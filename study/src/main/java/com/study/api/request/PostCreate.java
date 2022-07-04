156a5ed20e322cb6bb0fff62e6249467cc5695fe
tjrghk <tjrghk1992@gmail.com>
2022-07-04 15:15:27 +0900

    Hello World2

--- study/src/main/java/com/study/api/request/PostCreate.java
+++ study/src/main/java/com/study/api/request/PostCreate.java
@@ -27,7 +27,7 @@ public class PostCreate {
     public PostCreate(String title, String content) {
         this.title = title;
         this.content = content;
-        System.out.print("");
+        System.out.print("11");
     }
 
     public void validate() {
